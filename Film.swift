e6b882e3b108df5a0aa2836a4fdc3d4dce2ffa1e
makenaw <makena.walsh@gmail.com>
2016-01-19 19:08:29 +0800

    creates film entity

---
+++ Film.swift
@@ -0,0 +1,17 @@
+//
+//  Film.swift
+//  favoriteFilm
+//
+//  Created by makena  on 1/19/16.
+//  Copyright © 2016 makena . All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Film: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
