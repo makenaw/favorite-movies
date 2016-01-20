ceb55cb48d801107113fe8e86ed72fba7ac11d47
makenaw <makena.walsh@gmail.com>
2016-01-20 15:17:45 +0800

    adds film detail ui

---
+++ favoriteFilm/FilmDetailController.swift
@@ -0,0 +1,20 @@
+//
+//  FilmDetailController.swift
+//  favoriteFilm
+//
+//  Created by makena  on 1/20/16.
+//  Copyright © 2016 makena . All rights reserved.
+//
+
+import UIKit
+
+class FilmDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
